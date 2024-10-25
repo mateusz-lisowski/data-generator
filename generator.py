2f27c9af5da57e98424b0f123618fb6e180b2b3c
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-10-25 20:17:00 +0200

    Implement function for creating cities

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
+import csv
 import random
 from decimal import Decimal
-from uuid import uuid4
+from uuid import UUID, uuid4
+from dataclasses import dataclass, astuple
 
 from faker import Faker
 from faker.providers import DynamicProvider
@@ -25,3 +27,33 @@ show_type_provider = DynamicProvider(
 # Register custom faker providers
 fake.add_provider(show_type_provider)
 fake.add_provider(payment_type_provider)
+
+
+@dataclass
+class City:
+    id: UUID
+    name: str
+    population: int
+
+
+def generate_cities(quantity: int, output_file: str) -> list[UUID]:
+    cites_ids = []
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for _ in range(quantity):
+            city = City(
+                id=uuid4(),
+                name=fake.unique.city(),
+                population=random.randrange(100_000, 10_000_000)
+            )
+            writer.writerow(astuple(city))
+            cites_ids.append(city.id)
+    return cites_ids
+
+
+def main():
+    cites_ids = generate_cities(100, 'cities.csv')
+    print(cites_ids)
+
+if __name__ == '__main__':
+    main()
